948854aef551c5960b570e415d4cda8a0131cecf
ruben torres <ruben.torres@lifeomic.com>
2022-06-01 16:01:55 -0500

    chore: add pokemon project

---
+++ Pokemon/Pokemon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pokemon
+//
+//  Created by Ruben Torres on 1/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
